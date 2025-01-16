dd10f31f46e2ad6821b14c2e024878b2bd6aa1ac
rlarjsdn3 <rlarjsdn3@naver.com>
2025-01-16 13:33:31 +0900

    create pod project

---
+++ Example/SCLAlertView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SCLAlertView
+//
+//  Created by rlarjsdn3 on 01/16/2025.
+//  Copyright (c) 2025 rlarjsdn3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
